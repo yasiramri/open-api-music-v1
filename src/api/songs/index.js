ab4e3bd4935c4cc1556bf697ae141b33022c2b43
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-02-25 08:08:26 +0700

    update

---
+++ src/api/songs/index.js
@@ -0,0 +1,11 @@
+const SongsHandler_subm_v1 = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'songs',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const songsHandler_subm_v1 = new SongsHandler_subm_v1(service, validator);
+    server.route(routes(songsHandler_subm_v1));
+  },
+};
